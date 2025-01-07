0e9053fe7decfa38602ee6adb402accdaa23a915
cannorin <cannorin@users.noreply.github.com>
2025-01-08 01:48:03 +0900

    Add /arubinosky (2)

--- apps/web/src/routes/api/misskey/invite-code/+server.ts
+++ apps/web/src/routes/api/misskey/invite-code/+server.ts
@@ -3,29 +3,37 @@ import { RateLimiter } from "sveltekit-rate-limiter/server";
 
 import { dev } from "$app/environment";
 import { MISSKEY_API_KEY } from "$env/static/private";
-import { api } from "misskey-js";
+import type { InviteListResponse } from "misskey-js/entities.js";
 
 const limiter = new RateLimiter({
   IP: [10, "d"], // IP address limiter
   IPUA: [5, "10m"], // IP + User Agent limiter
 });
 
-const misskey = new api.APIClient({
-  origin: "https://misskey.cannorin.net",
-  credential: MISSKEY_API_KEY,
-});
-
 export async function GET(event: RequestEvent) {
   if (!dev && (await limiter.isLimited(event)))
     return new Response("Too many requests", { status: 429 });
 
-  const invites = await misskey.request("admin/invite/list", {
-    type: "unused",
-    sort: "+createdAt",
-    limit: 10,
-  });
-
-  const invite = invites.find((x) => !x.createdBy && !x.usedAt);
+  const res = await fetch(
+    "https://misskey.cannorin.net/api/admin/invite/list",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        i: MISSKEY_API_KEY,
+        type: "unused",
+        sort: "+createdAt",
+        limit: 10,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const json = await res.json();
+  console.log(json);
+  const invite = (json as InviteListResponse).find(
+    (x) => !x.createdBy && !x.usedAt,
+  );
   if (invite) return text(invite.code);
   return new Response("Not found", { status: 404 });
 }
