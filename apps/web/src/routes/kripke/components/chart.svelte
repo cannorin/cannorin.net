09f1aaf68b26342cf8df78fc4155238bfe0d57d9
cannorin <cannorin@users.noreply.github.com>
2025-09-24 20:38:23 +0900

    kripke: add stats (3)

--- apps/web/src/routes/kripke/components/chart.svelte
+++ apps/web/src/routes/kripke/components/chart.svelte
@@ -11,7 +11,7 @@ let { playerData }: Props = $props();
 
 const data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => ({
   move: i,
-  player: playerData[i] ?? 1,
+  player: playerData[i],
 }));
 </script>
 
